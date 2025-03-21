a1085f2eba7efef34d97c4fa762a0b82cf4b43c1
Rahul Lotlikar <lotlikarrahul47@gmail.com>
2025-03-20 22:51:29 -0400

    Update Product_of_Array_Except_Self.java

--- Product_of_Array_Except_Self.java
+++ Product_of_Array_Except_Self.java
@@ -4,10 +4,8 @@ class Solution {
         int[] res = new int[n];
         res[0]=1;
         for(int i = 1; i<n; i++){
-            res[i]= res[i-1]*nums[i-1];
-            
+            res[i]= res[i-1]*nums[i-1];  
         }
-        
         int r = 1;
         for(int i = n-1; i>=0; i--){
             res[i] = r*res[i];
